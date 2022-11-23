4926865f3918fe74dd509247e5e71c259a39c200
James Luo <contact@jamesckluo.ca>
2022-11-22 22:45:52 -0500

    Split main functions from server

---
+++ server.py
@@ -0,0 +1,20 @@
+#Set up Webhook using Flash
+#https://hackernoon.com/how-to-listen-for-webhooks-using-python-7g153uad
+from flask import Flask, request, Response, abort
+from flask_cors import CORS
+import flights_in_radius as FIR
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def respond():
+  if request.method == 'GET':
+    GPS_lat = float(request.args.get('GPS_lat'))
+    GPS_lon = float(request.args.get('GPS_lon'))
+    return FIR.main(GPS_lat,GPS_lon),200
+  else:
+    abort(400)
+
+if __name__ == '__main__':
+   app.run()
